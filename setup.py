6f9ea364a1b78c206e2e952132704c847a37b67d
Julien Maupetit <julien@maupetit.net>
2018-03-21 15:17:19 +0100

    feat(package) add setup script and configuration

--- setup.py
+++ setup.py
@@ -1,47 +1,5 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
-#
-# Copyright (C) 2014-2015 edX
-#
-# This software's license gives you freedom; you can copy, convey,
-# propagate, redistribute and/or modify this program under the terms of
-# the GNU Affero General Public License (AGPL) as published by the Free
-# Software Foundation (FSF), either version 3 of the License, or (at your
-# option) any later version of the AGPL published by the FSF.
-#
-# This program is distributed in the hope that it will be useful, but
-# WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero
-# General Public License for more details.
-#
-# You should have received a copy of the GNU Affero General Public License
-# along with this program in a file in the toplevel directory called
-# "AGPLv3". If not, see <http://www.gnu.org/licenses/>.
-#
-"""Set up for xblock-utils"""
-import os
-import os.path
 from setuptools import setup
 
-
-def package_data(pkg, root_list):
-    """Generic function to find package_data for `pkg` under `root`."""
-    data = []
-    for root in root_list:
-        for dirname, _, files in os.walk(os.path.join(pkg, root)):
-            for fname in files:
-                data.append(os.path.relpath(os.path.join(dirname, fname), pkg))
-
-    return {pkg: data}
-
-setup(
-    name='xblock-utils2',
-    version='0.2fun',
-    description='Various utilities for XBlocks',
-    packages=[
-        'xblockutils2',
-    ],
-    install_requires=[
-        'XBlock',
-    ],
-    package_data=package_data("xblockutils2", ["public", "templates"]),
-)
+setup()
